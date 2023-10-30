58e0c276e81bb74909699f4bdbe5b726ee8eab23
Kris Kurzawa <kurzawa1@yahoo.com>
2023-10-29 20:08:38 -0400

    initial commit, need to connect front to back

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const dotenv = require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const sendEmail = require('./utils/sendEmail')
+
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Home Page')
+})
+
+app.post('/api/sendemail', async (req, res) => {
+    const { emailTo } = req.body;
+
+
+    try {
+        const send_to = emailTo;
+        const sent_from = emailTo;
+        const subject = "Yo";
+        const message = `
+            <h3>Hello Zino</h3>
+            <p>Thanks for the Tutorials</p>
+            <p>Regards...</p>
+        `
+
+        await sendEmail(subject, message, send_to, sent_from)
+        res.status(200).json({ success: true, message: 'Email Sent' })
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}...`);
+    // console.log(dotenv)
+})
