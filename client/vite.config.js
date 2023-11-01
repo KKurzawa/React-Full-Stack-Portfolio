b5a0ba05992bb8a3948efadc958a22f814fc167e
Kris Kurzawa <kurzawa1@yahoo.com>
2023-11-01 13:34:26 -0400

    prep for heroku

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    open: true
+    open: true,
+    proxy: {
+      target: 'http://localhost:3001',
+      secure: false,
+      changeOrigin: true
+    }
   }
 })
--- server/server.js
+++ server/server.js
@@ -10,17 +10,17 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+}
+
 app.get('/', (req, res) => {
     res.send('Home Page')
 })
-
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '../client/dist')));
-
-//     app.get('*', (req, res) => {
-//       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-//     });
-//   } 
 
 app.post('/api/sendemail', async (req, res) => {
     try {
